29025e45240aa62c0d8cc6657952f09d2f8a3da6
Irina-Blazhevich <blazhevych.iryna@gmail.com>
2021-11-07 20:34:09 +0200

    added husky, eslint

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,35 +1,32 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 function Profile({ name, tag, location, avatar, stats }) {
-    return (
-    <div>
-        <div>
-            <img
-                src= {avatar}
-                alt="Аватар пользователя"
-                class="avatar"
-            />
-            <p class="name">{name}</p>
-            <p class="tag">{tag}</p>
-            <p class="location">{location}</p>
-        </div>
+  return (
+    <div className={s.card}>
+      <div className={s.container}>
+        <img src={avatar} alt="Аватар пользователя" className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
 
-        <ul class="stats">
-            <li>
-                <span class="label">Followers</span>
-                <span class="quantity">1000</span>
-            </li>
-            <li>
-                <span class="label">Views</span>
-                <span class="quantity">2000</span>
-            </li>
-            <li>
-                <span class="label">Likes</span>
-                <span class="quantity">3000</span>
-            </li>
-        </ul>
-        </div>
-    )
+      <ul className={s.stats}>
+        <li className={s.statsItem}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
+        </li>
+        <li className={s.statsItem}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
+        </li>
+        <li className={s.statsItem}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Profile;
